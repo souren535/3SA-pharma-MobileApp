ab1b0c0f2ceb1104ad14645c51a926fd1b5dfaa6
souren535 <souren.cool535@outlook.com>
2026-05-20 07:21:36 +0530

    change metro

--- metro.config.js
+++ metro.config.js
@@ -3,4 +3,18 @@ const { withNativeWind } = require("nativewind/metro");
 
 const config = getDefaultConfig(__dirname);
 
+// Ignore android and ios build directories to prevent Metro from crashing on FallbackWatcher
+const customBlockList = [
+  /.*\/android\/app\/.cxx\/.*/,
+  /.*\/android\/app\/build\/.*/,
+];
+
+if (Array.isArray(config.resolver.blockList)) {
+  config.resolver.blockList.push(...customBlockList);
+} else if (config.resolver.blockList) {
+  config.resolver.blockList = [config.resolver.blockList, ...customBlockList];
+} else {
+  config.resolver.blockList = customBlockList;
+}
+
 module.exports = withNativeWind(config, { input: "./global.css" });
